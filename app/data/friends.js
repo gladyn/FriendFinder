e181c1ac71b0845a56b6cc8180ab0be36b53c9c7
gladyn <glds.nvrr@gmail.com>
2019-05-22 01:40:32 -0700

    updated images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
       name: "Mary",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiEvJny3a7iAhVQwMQHHYmUCykQjRx6BAgBEAU&url=https%3A%2F%2Fwww.state.gov%2Fs%2Focr%2Fhispanic%2F2017%2F273860.htm&psig=AOvVaw0dmiDJoobdmD7kwqipwx5K&ust=1558600603782348",
       scores: [
         "5",
         "1",
@@ -17,7 +17,7 @@ var friends = [
     },
     {
       name: "Joe",
-      photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwivuKL63a7iAhXDiFQKHRlwD1sQjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.state.gov%252Fs%252Focr%252Fhispanic%252F285641.htm%26psig%3DAOvVaw0dmiDJoobdmD7kwqipwx5K%26ust%3D1558600603782348&psig=AOvVaw0dmiDJoobdmD7kwqipwx5K&ust=1558600603782348",
       scores: [
         "4",
         "2",
@@ -33,7 +33,7 @@ var friends = [
     },
     {
       name: "Lisa",
-      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi1oIyD3q7iAhVosVQKHUfiBvQQjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fdod.defense.gov%252FNews%252FArchive%252FTag%252F108263%252F%253FPage%253D2%26psig%3DAOvVaw0dmiDJoobdmD7kwqipwx5K%26ust%3D1558600603782348&psig=AOvVaw0dmiDJoobdmD7kwqipwx5K&ust=1558600603782348",
       scores: [
         "5",
         "2",
@@ -49,7 +49,7 @@ var friends = [
     },
     {
       name: "Mike",
-      photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjZ2-aN3q7iAhVsy1QKHZjJBR8QjRx6BAgBEAU&url=http%3A%2F%2Fwww.pdhealth.mil%2Fnews%2Fblog%2Fproviders-help-service-members-stay-mental-healthtreatment-across-settings-free-dod-program%3Fpage%3D11&psig=AOvVaw0dmiDJoobdmD7kwqipwx5K&ust=1558600603782348",
       scores: [
         "3",
         "3",
@@ -65,7 +65,7 @@ var friends = [
     },
     {
       name: "Kim",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiZg_iX3q7iAhUJsJ4KHWYIDcEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.state.gov%2Fs%2Focr%2Fhispanic%2F2017%2F273863.htm&psig=AOvVaw0dmiDJoobdmD7kwqipwx5K&ust=1558600603782348",
       scores: [
         "4",
         "3",
@@ -81,7 +81,7 @@ var friends = [
     },
     {
       name: "Ruben",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj8rLam3q7iAhXMsJ4KHZoEByUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.nps.gov%2Fkefj%2Fplanyourvisit%2Franger-programs.htm%3Fmaxrows%3D20%26keyword%3D%26hiderightrail%3Dtrue%26date_end%3D08%252F18%252F2018%26reinit%3Dfalse%26page%3D1%26repeat%3D1%26date_start%3D07%252F18%252F2018%26startrow%3D1&psig=AOvVaw0dmiDJoobdmD7kwqipwx5K&ust=1558600603782348",
       scores: [
         "4",
         "4",
